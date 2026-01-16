e75d95627eafbdb0396a297e82f4b8c2ca2f79b8
Kurt <breath103@gmail.com>
2026-01-16 19:11:09 +0900

    Add prepare script for GitHub installs

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./types";
-export * from "./record-query";
-export * from "./list-query";
-export * from "./repository";
-export * from "./react";
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./types";
-export * from "./record-query";
-export * from "./list-query";
-export * from "./repository";
-export * from "./react";
